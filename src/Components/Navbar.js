4e41cc8957d394c09d698af2d51e0fbfc21a6a90
Thando (Jayson) Mkhonza <95401051+Drakening@users.noreply.github.com>
2024-07-31 14:20:20 +0200

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,88 +1,58 @@
 import React, { useState } from "react";
+import { motion } from "framer-motion";
+import styles from "./Navbar.module.css";
 import Logo from "../Assets/Logo.svg";
 import { BsCart2 } from "react-icons/bs";
 import { HiOutlineBars3 } from "react-icons/hi2";
-import Box from "@mui/material/Box";
-import Drawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
-import HomeIcon from "@mui/icons-material/Home";
-import InfoIcon from "@mui/icons-material/Info";
-import CommentRoundedIcon from "@mui/icons-material/CommentRounded";
-import PhoneRoundedIcon from "@mui/icons-material/PhoneRounded";
-import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
-
-
+import { AiOutlineHome, AiOutlineInfoCircle, AiOutlineComment, AiOutlinePhone, AiOutlineShoppingCart } from "react-icons/ai";
 
 const Navbar = () => {
-    const [openMenu, setOpenMenu] = useState(false);
-    const menuOptions = [
-        {
-            text:"Home",
-            icon: <HomeIcon />
-        },
-        {
-            text:"About",
-            icon: <InfoIcon />
-        },
-        {
-            text:"Testimonials",
-            icon: <CommentRoundedIcon />
-        },
-        {
-            text:"Contact",
-            icon: <PhoneRoundedIcon />
-        },
-        {
-            text:"Cart",
-            icon: <ShoppingCartRoundedIcon />
-        },
-    ];
+  const [openMenu, setOpenMenu] = useState(false);
 
-    return (
-    <nav>
-        <div className='nav-logo-container'>
-            <img src={Logo} alt=''/>
-        </div>
-        <div className="navbar-links-container">
-        <a href="">Home</a>
-        <a href="">About</a>
-        <a href="">Testimonials</a>
-        <a href="">Contact</a>
-        <a href="">
-            <BsCart2 className="navbar-cart-icon" />
+  const menuOptions = [
+    { text: "Home", icon: <AiOutlineHome /> },
+    { text: "About", icon: <AiOutlineInfoCircle /> },
+    { text: "Testimonials", icon: <AiOutlineComment /> },
+    { text: "Contact", icon: <AiOutlinePhone /> },
+    { text: "Cart", icon: <AiOutlineShoppingCart /> },
+  ];
+
+  return (
+    <nav className={styles.nav}>
+      <div className={styles["nav-logo-container"]}>
+        <img src={Logo} alt="Logo" />
+      </div>
+      <div className={styles["navbar-links-container"]}>
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#testimonials">Testimonials</a>
+        <a href="#contact">Contact</a>
+        <a href="#cart">
+          <BsCart2 className={styles["navbar-cart-icon"]} />
         </a>
-        <button className="primary-button">Bookings Now</button>
-        </div>
-        <div className="navbar-menu-container">
+        <button className={styles["primary-button"]}>Bookings Now</button>
+      </div>
+      <div className={styles["navbar-menu-container"]}>
         <HiOutlineBars3 onClick={() => setOpenMenu(true)} />
-        </div>
+      </div>
 
-        <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right" >
-        <Box sx={{ width: 250}}
-        role="presentation"
-        onClick={() => setOpenMenu(false)}
-        onKeyDown={() => setOpenMenu(false)}
+      {openMenu && (
+        <motion.div
+          className={styles.drawer}
+          initial={{ x: '100%' }}
+          animate={{ x: 0 }}
+          exit={{ x: '100%' }}
         >
-        <List>
-            {menuOptions.map((item) => (
-                <ListItem key={item.text} disablePadding>
-                    <ListItemButton>
-                        <ListItemIcon>{item.icon}</ListItemIcon>
-                        <ListItemText primary={item.text} />
-                    </ListItemButton>
-                </ListItem>
-            ) )}
-        </List>
-        <Divider />
-        </Box>
-        </Drawer>
+          {menuOptions.map((item) => (
+            <div key={item.text} onClick={() => setOpenMenu(false)}>
+              <div>{item.icon}</div>
+              <span>{item.text}</span>
+            </div>
+          ))}
+        </motion.div>
+      )}
     </nav>
-    )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
