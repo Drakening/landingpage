4a28a38b143e43956168a403fda9b6b5c6fcfe15
Thando (Jayson) Mkhonza <95401051+Drakening@users.noreply.github.com>
2024-07-31 14:22:14 +0200

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <div className="contact-page-wrapper">
         <h1 className="primary-heading">Have Question In Mind?</h1>
-        <h1 className="primary-heading">Let Us Help You</h1>
+        <h1 className="primary-heading">Let me Help You</h1>
         <div className="contact-form-container">
             <input type="text" placeholder="yourmail@gmsil.com"/>
             <button className="secondary-button">Submit</button>
